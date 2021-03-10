b098c29f778be5f8d844e8d8b36562260acaa1d3
JenniD5 <elizabethduran0105@gmail.com>
2021-03-09 18:08:57 -0700

    frombuffer_09-03-2021

--- readframes.py
+++ readframes.py
@@ -1,12 +1,13 @@
 import wave
+import numpy as np 
 #cargar archivo wav en la variable 
 
 godmorning=wave.open('good-morningMan.wav','r')
 
 frames = godmorning.readframes(-1)
 #obtener todos los frames del objeto wave
-
+ondaconvertida = np.frombuffer(frames, dtype='int16')
 #mostrar el resultado de frames
-print(frames[:100])
+print(ondaconvertida[:10])
 
 
